7996d767658d090da98241a0af081f6955d468a8
A.Jelouani <jelouaniabdelghani@gmail.com>
2022-11-09 18:09:35 +0100

    add petit style

--- css/script.js
+++ css/script.js
@@ -1,66 +1,98 @@
-function confirmer() {
-    var res = confirm("Êtes-vous sûr de commander?");
-}
-
 
 document.querySelector("#hamberger").addEventListener("click", function() {
-    document.querySelector(".nav2-menu").classList.toggle("active");
+    document.querySelector(".nav_bar").classList.toggle("active");
 
 })
+
+document.querySelector("#hamberger").addEventListener("click", function() {
+    // document.querySelector(".search").style.display = "none";
+    document.querySelector(".search").classList.toggle("active");
+})
+
+
+
+// document.querySelector(".crt").addEventListener("click", function() {
+//     document.querySelector(".products-container").style.display= "block";
+
+// })
+
+
+
+
+// filter
+
+document.querySelector(".A1").addEventListener("click", function () {
+    document.querySelector(".toutes-salade").style.display = "flex";
+    document.querySelector(".tout-plats").style.display = "none";
+    document.querySelector(".tout-obento").style.display = "none";
+})
+document.querySelector(".A2").addEventListener("click", function () {
+    document.querySelector(".toutes-salade").style.display = "none";
+    document.querySelector(".tout-plats").style.display = "flex";
+    document.querySelector(".tout-obento").style.display = "none";
+})
+document.querySelector(".A3").addEventListener("click", function () {
+    document.querySelector(".toutes-salade").style.display = "none";
+    document.querySelector(".tout-plats").style.display = "none";
+    document.querySelector(".tout-obento").style.display = "flex";
+})
+// for show all
+document.querySelector(".A4").addEventListener("click", function () {
+    document.querySelector(".toutes-salade").style.display = "flex";
+    document.querySelector(".tout-plats").style.display = "flex";
+    document.querySelector(".tout-obento").style.display = "flex";
+})
+
+// hhhhhhhhhhhh
+
+
 
 let carts = document.querySelectorAll('.add-cart');
 
 
 let maklas = [{
-        id: 1,
         name: 'salade concombre soja poivron',
         price: 42,
         imgfood: "images/salade-concombresojapoivron-a-la-japonaise--278713p453800.jpg",
         num: 0,
     },
     {
-        id: 1,
         name: 'salade de chou blanc et carotte',
         price: 40,
         imgfood: "images/salade_de_chou_blanc_et_carotte.jpg",
         num: 0,
     },
     {
-        id: 1,
         name: 'salade dalgues',
         price: 46,
         imgfood: "images/salade d'algues.webp",
         num: 0,
     },
     {
-        id: 1,
         name: 'salade de chou et daikon',
         price: 54,
         imgfood: "images/salade-de-chou-et-daikon-a-la-japonaise.jpeg",
         num: 0,
     },
-
-
     {
-        id: 2,
         name: 'Plat Gyoza',
         price: 42,
         imgfood: "images/Gyoza.jpg",
         num: 0,
     }, {
-        id: 2,
+      
         name: 'Plat Okonomiyaki',
         price: 40,
         imgfood: "images/Okonomiyaki.jpg",
         num: 0,
     }, {
-        id: 2,
+       
         name: 'Plat Yakitori',
         price: 46,
         imgfood: "images/Yakitori.png",
         num: 0,
     }, {
-        id: 2,
+       
         name: 'Plat Tonkatsu',
         price: 54,
         imgfood: "images/Tonkatsu.jpg",
@@ -69,25 +101,25 @@ let maklas = [{
 
 
     {
-        id: 3,
+      
         name: 'Gyoza',
         price: 42,
         imgfood: "images/Gyoza.jpg",
         num: 0,
     }, {
-        id: 3,
+        
         name: 'Osaka Cooking Class',
         price: 40,
         imgfood: "images/Osaka Cooking Class.png",
         num: 0,
     }, {
-        id: 3,
+        
         name: 'Obento art culinaire',
         price: 46,
         imgfood: "images/obento- art culinaire.jpg",
         num: 0,
     }, {
-        id: 3,
+       
         name: 'Box Guide',
         price: 54,
         imgfood: "images/Obento Box Guide.jpg",
@@ -114,10 +146,10 @@ function cartNumbers(makla) {
 
     if (maklas) {
         localStorage.setItem('cartNumbers', maklas + 1);
-        document.querySelector('.cart span').textContent = maklas + 1;
+        document.querySelector('.shop sup').textContent = maklas + 1;
     } else {
         localStorage.setItem('cartNumbers', 1);
-        document.querySelector('.cart span').textContent = 1;
+        document.querySelector('.shop sup').textContent = 1;
     }
     setItem(makla);
 }
@@ -167,7 +199,7 @@ function displayCart() {
         Object.values(cartItems).map(item => {
             maklacont.innerHTML += `
             <div class="product">
-                <ion-icon name="close-circle"></ion-icon>
+                <ion-icon name="close-circle" class="close11"></ion-icon>
                 <img src="${item.imgfood}" class="circular--square" id="img-acht">
                 <span>${item.name}</span>
             </div>
@@ -193,50 +225,4 @@ function displayCart() {
 }
 displayCart();
 
-// const categoryTitle = document.querySelectorAll('#type-menu option');
-// const allCategoryPosts = document.querySelectorAll('.all');
-// console.log(categoryTitle);
-// console.log(allCategoryPosts);
-// for (let i = 0; i < categoryTitle.length; i++) {
-//     categoryTitle[i].addEventListener('click', filterPosts.bind(this, categoryTitle[i]));
-// }
 
-// function filterPosts(item) {
-//     changeActivePosition(item);
-//     for (let i = 0; i < allCategoryPosts.length; i++) {
-//         if (allCategoryPosts[i].classList.contains(item.attributes.id.value)) {
-//             allCategoryPosts[i].style.display = "block";
-//         } else {
-//             allCategoryPosts[i].style.display = "none";
-//         }
-//     }
-// }
-
-// function changeActivePosition(activeItem) {
-//     for (let i = 0; i < categoryTitle.length; i++) {
-//         categoryTitle[i].classList.remove('active');
-//     }
-//     activeItem.classList.add('active');
-// };
-
-document.querySelector(".A1").addEventListener("click", function() {
-    document.querySelector("body > section.menu > div.menu-food all salades ").style.display = "flex";
-    document.querySelector("body > div.mother > div.menu-food all plats ").style.display = "none";
-    document.querySelector("body > div.mother > div.menu-food all obento ").style.display = "none";
-})
-document.querySelector(".A2").addEventListener("click", function() {
-    document.querySelector("body > div.mother > div.card1 ").style.display = "none";
-    document.querySelector("body > div.mother > div.card2 ").style.display = "flex";
-    document.querySelector("body > div.mother > div.card3 ").style.display = "none";
-})
-document.querySelector(".A3").addEventListener("click", function() {
-        document.querySelector("body > div.mother > div.card1 ").style.display = "none";
-        document.querySelector("body > div.mother > div.card2 ").style.display = "none";
-        document.querySelector("body > div.mother > div.card3 ").style.display = "flex";
-    })
-    // for show all
-document.querySelector(".A4").addEventListener("click", function() {
-    document.querySelector("body > div.mother > div.card1 ").style.display = "flex";
-    document.querySelector("body > div.mother > div.card2 ").style.display = "flex";
-    document.querySelector("body > div.mother > div.card3 ").style.display = "flex";
-})
