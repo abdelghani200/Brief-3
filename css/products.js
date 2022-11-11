1b0550a090e32e56c31f2fbb61d8f8e6ee47ca5a
jelouani <jelouaniabdelghani@gmail.com>
2022-11-11 01:12:32 +0100

    finiched

---
+++ css/products.js
@@ -0,0 +1,64 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  1: {
+    name : "salade concombre soja poivron",
+    img : "salade-concombresojapoivron-a-la-japonaise--278713p453800.jpg",
+    price : 28
+  },
+  2: {
+    name : "salade de chou blanc et carotte",
+    img : "salade_de_chou_blanc_et_carotte.jpg",
+    price : 34
+  },
+  3: {
+    name : "salade de chou et daikon",
+    img : "salade-de-chou-et-daikon-a-la-japonaise.jpeg",
+    price : 67
+  },
+  4:{
+    name: "salade de chou et daikon",
+    img:"salade-de-chou-et-daikon-a-la-japonaise.jpeg",
+    price: 25,
+  },
+  5: {
+    name : "Plat Gyoza",
+    img : "Gyoza.jpg",
+    price : 45
+  },
+  6:{
+    name : "Plat Okonomiyaki",
+    img : "Okonomiyaki.jpg",
+    price : 43
+  },
+  7:{
+    name : "Plat Yakitori",
+    img : "Yakitori.png",
+    price : 31
+  },
+  8:{
+    name: 'Plat Tonkatsu',
+    img:"Tonkatsu.jpg",
+    price: 24,
+  },
+  9: {
+    name : "Plat Gyoza",
+    img : "Gyoza.jpg",
+    price : 55
+  },
+  10:{
+    name : "Osaka Cooking Class",
+    img : "Osaka Cooking Class.png",
+    price : 46
+  },
+  11:{
+    name : "Obento art culinaire",
+    img : "obento- art culinaire.jpg",
+    price : 56
+  },
+  12:{
+    name : "Box Guide",
+    img : "Obento Box Guide.jpg",
+    price : 34
+  }
+};
+ 
